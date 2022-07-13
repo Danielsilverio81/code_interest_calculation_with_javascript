e6766e9a158ef980219a3b14200fc69e2b1ab4e7
Daniel silverio <danielsilverio81@outlook.com>
2022-07-13 18:42:12 -0300

    commit_in_indexjs

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,7 +1,43 @@
 import inputText from 'readline-sync';
 
-let valorDivida = inputText.question('Informe o valor devido: ');
-let diasAtraso = inputText.question('Informe quantos dias se passaram deis do vencimento do boleto: ');
+console.log("App Calculo de Juros");
 
-console.log("Valor original da divida: "+valorDivida);
-console.log("Dias em atraso: "+diasAtraso);
+let juros;
+let valorDivida = inputText.question('Informe o valor devido: ');
+
+if(valorDivida > 0) {
+    let diasAtraso = inputText.question('Informe quantos dias se passaram deis do vencimento do boleto: ');
+    if(diasAtraso > 0) {
+
+        if(diasAtraso > 15) {
+
+            juros = 10
+
+        } else {
+
+            juros = 5
+
+        }
+
+        valorDivida = Number(valorDivida);
+        diasAtraso = Number(diasAtraso);
+
+        let totalJuros = (valorDivida / 100) * juros;
+        let valorTotal = valorDivida + totalJuros;
+
+        console.log("Taxa de juros: "+juros+"%");
+        console.log("Valor total com juros: "+valorTotal);
+    } else if(diasAtraso == 0) {
+
+        console.log("Você esta em dia continue assim e estará no controle da sua vida financeira!!!");
+
+    } else {
+        console.log("O valor digitado não é um numero!!!");
+    }
+} else if(valorDivida == 0) {
+
+    console.log("Valor digitado deve ser maior que zero!!!");
+    
+} else {
+    console.log("O valor digitado não é um numero!!!");
+}
